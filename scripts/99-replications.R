88663b60e3a2aa10b97c4ecbe5861cd37bbe7710
“Qin <thisisheqins@gmail.com>
2024-04-19 01:17:36 +0800

    update

--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -1,19 +1,25 @@
-#### Preamble ####
-# Purpose: Replicated graphs from... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+library(readr)
+library(dplyr)
+library(knitr)
+library(kableExtra)
 
+# Read the dataset
+data <- read.csv(here::here("data/cleaned_data.csv"))
 
-#### Workspace setup ####
-library(tidyverse)
-# [...UPDATE THIS...]
+# Reshape data to wide format
+wide_data <- data %>% 
+  filter(products_and_product_groups %in% c('Food', 'All-items')) %>%
+  spread(key = products_and_product_groups, value = value) %>%
+  rename(Year = ref_date)
 
-#### Load data ####
-# [...ADD CODE HERE...]
+# Assuming that the 'value' columns are already in the correct format and do not need additional formatting
+# If formatting is needed, include the appropriate mutate() calls here
 
+# Create the table
+wide_data_display <- wide_data %>%
+  select(Year, `Food`, `All-items`) %>% 
+  rename(`Food Value` = Food, `All-items Value` = `All-items`)
 
-
+# Render the table using knitr::kable and kableExtra::kable_styling
+wide_data_display %>%
+  kable()
