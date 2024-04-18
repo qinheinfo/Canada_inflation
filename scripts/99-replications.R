6bcd6af17f9f12400b858011e87114bc0a767fac
“Qin <thisisheqins@gmail.com>
2024-04-19 01:18:01 +0800

    test

--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -23,3 +23,36 @@ wide_data_display <- wide_data %>%
 # Render the table using knitr::kable and kableExtra::kable_styling
 wide_data_display %>%
   kable()
+
+
+```{r}
+#| messages: false
+#| echo: false
+#| warning: false
+#| label: fig-2
+#| fig-cap: "Cost of Bacon Rate vs. Inflation Rate, data (since 2018)"
+#| fig-align: center
+#| fig-width: 10
+#| fig-height: 3
+#### Create graphs plotting mandated and actual response times ####
+bacon_table_display <- bacon_inflation_data |>
+  # Order the columns 
+  select(year, bacon, percent_change, inflation_rate) |> 
+  
+  # Change months to fiscal quarters
+  mutate(bacon =  paste("\\$", bacon, sep = "")) |>
+  mutate(percent_change =  paste(percent_change, "\\%", sep = "")) |>
+  mutate(inflation_rate =  paste(inflation_rate, "\\%", sep = "")) |>
+  
+  # Rename the columns
+  rename(
+    "Year" = "year", 
+    "Bacon Cost" = "bacon", 
+    "Inflation Rate" = "inflation_rate", 
+    "Bacon Cost Rate" = "percent_change"
+  )
+
+bacon_table_display |>
+  knitr::kable(align = "l", booktabs = TRUE, escape = FALSE, linesep = "" )|>
+  kable_styling(font_size = 10, latex_options = "hold_position", full_width = TRUE)
+```
